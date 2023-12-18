38fe649c9e4b63ed6f3170d4a1306426074227a6
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2023-12-18 14:35:01 +0700

    Upload product services [CREATE]

--- src/router/access/index.js
+++ src/router/access/index.js
@@ -6,11 +6,11 @@ const accessController = require("../../controllers/access.controllers")
 const { asyncHandler } = require("../../helpers/asyncHandler")
 const { authentication } = require("../../auth/authUtils")
 
-//signUp
+//  signUp
 router.post('/shop/signup', asyncHandler(accessController.signUp))
 router.post('/shop/login', asyncHandler(accessController.login))
 
-//authentication - check accessToken is legitable or not - prevent a sabotage
+//  authentication - check accessToken is legitable or not - prevent a sabotage
 router.use(authentication)
 router.post('/shop/logout', asyncHandler(accessController.logout))
 router.post('/shop/handlerRefreshToken', asyncHandler(accessController.handlerRefreshToken))
