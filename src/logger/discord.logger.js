e387ed883138be008c2f5915fcee0ffd3a7fc7d1
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2024-01-08 23:57:57 +0700

    Redis pub/sub services for incoming in use

---
+++ src/logger/discord.logger.js
@@ -0,0 +1,29 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('../../config.json');
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+});
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+client.on("messageCreate", (msg) => {
+    if (msg.author.bot) return;
+    if (msg.content === "hello") {
+        msg.reply("Hello there ! ")
+    }
+})
